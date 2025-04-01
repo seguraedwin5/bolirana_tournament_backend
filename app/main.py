91e29006a43dd264cc2d8d6eaab0f7456a5694c6
Edwin Segura <seguraedwin5@gmail.com>
2025-03-31 19:22:52 -0500

    feature: se implementan las acciones de post y get by id y conexion con la base de datos usando herencia

--- app/main.py
+++ app/main.py
@@ -1,15 +1,32 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException, Query
 from sqlmodel import Field, Session, SQLModel, create_engine, select
 from contextlib import asynccontextmanager
 sqlite_file_name = "database.db"
 sqlite_url = f"sqlite:///{sqlite_file_name}"
 
-class Jugador(SQLModel, table=True):
-    id: int | None = Field(default=None, primary_key=True)
-    nombre: str
-    edad: int
+# modelos de la aplicacion
+class JugadorBase(SQLModel):
+    nombre: str = Field(index=True)
+    edad: int | None = Field(default=None, index=True)
     nickname: str
 
+
+class Jugador(JugadorBase, table=True):
+    id: int | None = Field(default=None, primary_key=True)
+    
+
+class JugadorCreate(JugadorBase):
+    pass
+
+class JugadorPublic(JugadorBase):
+    id:int
+
+class JugadorUpdate(SQLModel):
+    #pendiente de implementacion
+    pass 
+
+
+# Configuracion de la base de datos
 connect_args = {"check_same_thread": False}
 engine = create_engine(sqlite_url, connect_args=connect_args)
 
@@ -32,16 +49,25 @@ app = FastAPI(lifespan=lifespan)
 def read_root():
     return {"Hello": "World"}
 
-@app.post("/jugadores/", response_model=Jugador)
-def create_jugador(jugador: Jugador):
+@app.post("/jugadores/", response_model=JugadorPublic)
+def create_jugador(jugador: JugadorCreate):
     with Session(engine) as session:
-        session.add(jugador)
+        db_jugador = Jugador.model_validate(jugador)
+        session.add(db_jugador)
         session.commit()
-        session.refresh(jugador)
-    return jugador
+        session.refresh(db_jugador)
+    return db_jugador
 
-@app.get("/jugadores/", response_model=list[Jugador])
-def read_jugadores():
+@app.get("/jugadores/", response_model=list[JugadorPublic])
+def read_jugadores(offset: int = 0, limit: int = Query(default=100, le=100)):
     with Session(engine) as session:
-        jugadores = session.exec(select(Jugador)).all()
+        jugadores = session.exec(select(Jugador).offset(offset).limit(limit)).all()
     return jugadores
+
+@app.get("/jugadores/{jugador_id}", response_model=JugadorPublic)
+def read_jugador(jugador_id: int):
+    with Session(engine) as session:
+        jugador = session.get(Jugador, jugador_id)
+        if not jugador:
+            raise HTTPException(status_code=404, detail="Jugador no encontrado")
+        return jugador
