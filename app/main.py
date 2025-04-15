6a9a2ed516066130298a0c7d89b1a919e135d879
Edwin Segura <seguraedwin5@gmail.com>
2025-04-15 00:31:40 -0500

    feature:se ajustan dependencias y update de jugador

--- app/main.py
+++ app/main.py
@@ -1,21 +1,10 @@
-from fastapi import FastAPI, HTTPException, Query
-from sqlmodel import  Session, create_engine, select
+from fastapi import FastAPI, HTTPException, Query, Depends
+from sqlmodel import  Session, select
 from contextlib import asynccontextmanager
-from models import Jugador, JugadorCreate, JugadorPublic, JugadorUpdate
-from sqlmodel import SQLModel
+from .models import Jugador, JugadorCreate, JugadorPublic, JugadorUpdate
+from .dependencies import get_session, create_db_and_tables, engine
 
 
-sqlite_file_name = "database.db"
-sqlite_url = f"sqlite:///{sqlite_file_name}"
-
-
-
-# Configuracion de la base de datos
-connect_args = {"check_same_thread": False}
-engine = create_engine(sqlite_url, connect_args=connect_args)
-
-def create_db_and_tables():
-    SQLModel.metadata.create_all(engine)
 
 
 @asynccontextmanager
@@ -37,51 +26,79 @@ app = FastAPI(lifespan=lifespan)
 def read_root():
     return {"Hello": "World"}
 
+
 @app.post("/jugadores/", response_model=JugadorPublic)
-def create_jugador(jugador: JugadorCreate):
+def create_jugador(*,session:Session = Depends(get_session) ,jugador: JugadorCreate):
     hashed_password = hash_password(jugador.password) # hasheamos la contraseña
-    with Session(engine) as session:
-        extra_data = {"hashed_password": hashed_password} # creamos un diccionario con los datos extra
-        db_jugador = Jugador.model_validate(jugador, update=extra_data) # creamos un objeto Jugador con los datos del cliente y los datos extra
-        session.add(db_jugador)
-        session.commit()
-        session.refresh(db_jugador)
+    extra_data = {"hashed_password": hashed_password} # creamos un diccionario con los datos extra
+    db_jugador = Jugador.model_validate(jugador, update=extra_data) # creamos un objeto Jugador con los datos del cliente y los datos extra
+    session.add(db_jugador)
+    session.commit()
+    session.refresh(db_jugador)
     return db_jugador
 
+
 @app.get("/jugadores/", response_model=list[JugadorPublic])
-def read_jugadores(offset: int = 0, limit: int = Query(default=100, le=100)):
-    with Session(engine) as session:
-        jugadores = session.exec(select(Jugador).offset(offset).limit(limit)).all()
+def read_jugadores( 
+    *,
+    session:Session = Depends(get_session),
+    offset: int = 0,
+    limit: int = Query(default=100, le=100)
+    ):
+    """Obtiene una lista de jugadores paginada"""
+    jugadores = session.exec(select(Jugador).offset(offset).limit(limit)).all()
     return jugadores
 
+
 @app.get("/jugadores/{jugador_id}", response_model=JugadorPublic)
-def read_jugador(jugador_id: int):
+def read_jugador(
+    *,
+    session:Session = Depends(get_session),
+    jugador_id: int
+    ):
     with Session(engine) as session:
         jugador = session.get(Jugador, jugador_id)
         if not jugador:
             raise HTTPException(status_code=404, detail="Jugador no encontrado")
         return jugador
-    
+
+
 @app.patch("/jugadores/{jugador_id}", response_model=JugadorPublic, description="Actualiza un jugador existente")
-def update_jugador(jugador_id: int, jugador: JugadorUpdate):
-    with Session(engine) as session:
-        db_jugador = session.get(Jugador, jugador_id)
-        if not db_jugador:
-            raise HTTPException(status_code=404, detail="Jugador no encontrado")
-        
-        jugador_data = jugador.model_dump(exclude_unset=True) # se crea un diccionario con los datos a actualizar enviado por el cliente
-        extra_data = {}
-        if "password" in jugador_data: # si la contraseña es parte de los datos a actualizar, se hashea
-            password = jugador_data["password"]
-            hashed_password = hash_password(password)
-            extra_data["hashed_password"] = hashed_password
-        db_jugador.sqlmodel_update(jugador_data, update=extra_data) # actualiza el objeto de la base de datos con los datos del cliente
-        session.add(db_jugador) # se añade el objeto a la sesion para que sea actualizado
-        session.commit() ## se hace el commit para guardar los cambios en la base de datos
-        session.refresh(db_jugador) # se refresca el objeto para que tenga los datos actualizados de la base de datos
+def update_jugador(
+    *,
+    session:Session = Depends(get_session),
+    jugador_id: int,
+    jugador: JugadorUpdate
+    ):
+    db_jugador = session.get(Jugador, jugador_id)
+    if not db_jugador:
+        raise HTTPException(status_code=404, detail="Jugador no encontrado")
     
+    jugador_data = jugador.model_dump(exclude_unset=True) # se crea un diccionario con los datos a actualizar enviado por el cliente
+    extra_data = {}
+    if "password" in jugador_data: # si la contraseña es parte de los datos a actualizar, se hashea
+        password = jugador_data["password"]
+        hashed_password = hash_password(password)
+        extra_data["hashed_password"] = hashed_password
+    db_jugador.sqlmodel_update(jugador_data, update=extra_data) # actualiza el objeto de la base de datos con los datos del cliente
+    session.add(db_jugador) # se añade el objeto a la sesion para que sea actualizado
+    session.commit() ## se hace el commit para guardar los cambios en la base de datos
+    session.refresh(db_jugador) # se refresca el objeto para que tenga los datos actualizados de la base de datos
     return db_jugador
 
+
+@app.delete("/jugadores/{jugador_id}", response_model=JugadorPublic, description="Elimina un jugador existente")
+def delete_jugador(
+    *,
+    session:Session = Depends(get_session),
+    jugador_id: int
+    ):
+    db_jugador = session.get(Jugador, jugador_id)
+    if not db_jugador:
+        raise HTTPException(status_code=404, detail="Jugador no encontrado")
+    session.delete(db_jugador)
+    session.commit()
+    return { "ok": True, "message": "Jugador eliminado"}
 
 
 
--- app/models.py
+++ app/models.py
@@ -1,16 +1,39 @@
-from sqlmodel import SQLModel, Field
+from sqlmodel import SQLModel, Field, Relationship
 # modelos de la aplicacion
+
+# modelos de Equipos
+
+class EquipoBase(SQLModel):
+    nombre: str = Field(index=True)
+    ciudad: str = Field(index=True)
+
+class Equipo(EquipoBase, table=True):
+    id: int | None = Field(default=None, primary_key=True)
+    jugadores: list["Jugador"] = Relationship(back_populates="equipo")
+
+class EquipoCreate(EquipoBase):
+    pass
+
+class EquipoPublic(EquipoBase):
+    id: int
+
+class EquipoUpdate(SQLModel):
+    nombre: str | None = None
+    ciudad: str | None = None
+     
 
 class JugadorBase(SQLModel):
     nombre: str = Field(index=True)
     edad: int | None = Field(default=None, index=True)
     nickname: str
 
+    equipo_id: int | None = Field(default=None, foreign_key="equipo.id")
 
 class Jugador(JugadorBase, table=True):
     id: int | None = Field(default=None, primary_key=True)
     hashed_password: str = Field()
-
+    equipo: Equipo | None = Relationship(back_populates="jugadores")
+    
 class JugadorCreate(JugadorBase):
     password : str 
 
@@ -22,4 +45,3 @@ class JugadorUpdate(SQLModel):
     edad: int | None = None
     nickname: str | None = None
     password: str | None = None
-    
